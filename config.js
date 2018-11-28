17f31a68a7bb86c676c4e9d9c8a04918c163716b
Joaquin Fox <joaquin.fox@gmail.com>
2018-11-28 15:50:14 -0700

    redirect config to mlab

--- config.js
+++ config.js
@@ -2,6 +2,9 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/noteful',
-  TEST_MONGODB_URI: 'mongodb://localhost:27017/noteful-test'
+  // MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/noteful',
+  MONGODB_URI:
+    process.env.MONGODB_URI ||
+    'mongodb://dev:password1@ds163745.mlab.com:63745/noteful',
+  TEST_MONGODB_URI: 'mongodb://dev:password1@ds163745.mlab.com:63745/noteful'
 };
