e275dfe187dabf009553abbd45c31cbb9257e818
Joaquin Fox <joaquin.fox@gmail.com>
2018-11-29 13:21:50 -0700

    formatted seed-database and confirmed seeding properly

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -3,14 +3,26 @@ const mongoose = require('mongoose');
 
 const { MONGODB_URI } = require('../config');
 const Note = require('../models/note');
+const Folder = require('../models/folder');
+const { notes, folders } = require('../db/seed/data');
 
-const { notes } = require('../db/seed/notes');
-
-mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+mongoose
+  .connect(
+    MONGODB_URI,
+    { useNewUrlParser: true }
+  )
   .then(() => mongoose.connection.db.dropDatabase())
-  .then(() => Note.insertMany(notes))
+  .then(() => {
+    return Promise.all([
+      Note.insertMany(notes),
+      Folder.insertMany(folders),
+      Folder.createIndexes()
+    ]);
+  })
   .then(results => {
-    console.info(`Inserted ${results.length} Notes`);
+    //this is now an array b/c of the definition in the then above.
+    console.info(`Inserted ${results[0].length} Notes`);
+    console.info(`Inserted ${results[1].length} Folders`);
   })
   .then(() => mongoose.disconnect())
   .catch(err => {
